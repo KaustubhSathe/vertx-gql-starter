685df88eec3c80161065a6dec3f4cfd79c67fb9f
kaustubhsathefc <89383411+kaustubhsathefc@users.noreply.github.com>
2021-12-30 12:56:42 +0530

    change

--- src/main/java/AbstractGraphQLVerticle.java
+++ src/main/java/AbstractGraphQLVerticle.java
@@ -12,7 +12,15 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.rxjava3.core.AbstractVerticle;
 import io.vertx.rxjava3.core.http.HttpServer;
+import io.vertx.rxjava3.core.http.HttpServerRequest;
 import io.vertx.rxjava3.ext.web.Router;
+import io.vertx.rxjava3.ext.web.handler.BodyHandler;
+import io.vertx.rxjava3.ext.web.handler.ResponseContentTypeHandler;
+import io.vertx.rxjava3.ext.web.handler.StaticHandler;
+import io.vertx.rxjava3.ext.web.handler.graphql.GraphQLHandler;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -20,6 +28,7 @@ import java.nio.file.Paths;
 import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
 import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
 
+@Slf4j
 public class AbstractGraphQLVerticle extends AbstractVerticle {
     private final String packageName;
     private HttpServer httpServer;
@@ -42,39 +51,47 @@ public class AbstractGraphQLVerticle extends AbstractVerticle {
                 .setUseAlpn(httpConfig.isUseAlpn());
 
         Router router = getRouter();
+        val server = vertx.createHttpServer(options);
+        val handleRequests = server.requestStream()
+                .toFlowable()
+                .map(HttpServerRequest::pause)
+                .onBackpressureDrop(req -> {
+                    log.error("Dropping request with status 503");
+                    req.response().setStatusCode(503).end();
+                })
     }
 
     protected Router getRouter(){
         Router router = Router.router(this.vertx);
-        router.route()
+        router.route().handler(BodyHandler.create());
+        router.route().handler(ResponseContentTypeHandler.create());
+        router.route().handler(StaticHandler.create());
+        router.get("/liveness").handler(ctx -> ctx.response().end("Success!"));
+        var graphQlHandler = GraphQLHandler.create(setupGraphQL());
+        router.route("/graphql").handler(graphQlHandler);
+        return router;
     }
 
     private GraphQL setupGraphQL(){
-        try(var paths = Files.walk(Paths.get("/schemas"))){
+        try(var paths = Files.walk(Paths.get("/graphqls"))){
             TypeDefinitionRegistry typeDefinitionRegistry = new TypeDefinitionRegistry();
             SchemaParser schemaParser = new SchemaParser();
             SchemaGenerator schemaGenerator = new SchemaGenerator();
             paths.filter(path -> com.google.common.io.Files.getFileExtension(path.getFileName().toString()) == "graphqls")
                         .forEach(x -> typeDefinitionRegistry.merge(schemaParser.parse(x.toFile())));
             var runtimeWiring = newRuntimeWiring()
-                    .type(newTypeWiring("Query"));
+                    .type(newTypeWiring("Query")
+                            .dataFetcher(() -> ()));
             GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry,runtimeWiring);
+            return GraphQL.newGraphQL(graphQLSchema).build();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
-
-
     @Override
     public Completable rxStart() {
         return super.rxStart();
-    }
-
-    private GraphQL setupGraphQL(){
-
-
     }
 
     @Override
